7821689093d788a61db60e9a846f5bd2512434c7
Jeremy Pritts <jrpritts7@gmail.com>
2021-11-20 02:03:38 -0500

    endian reader and writer

---
+++ MeshSharp/IO/EndianReader.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+using System.Text;
+
+namespace MeshSharp.IO
+{
+	internal class EndianReader : BinaryReader
+	{
+		public EndianReader(Stream input) : base(input) { }
+		public EndianReader(Stream input, Encoding encoding) : base(input, encoding) { }
+		public EndianReader(Stream input, Encoding encoding, bool leaveOpen) : base(input, encoding, leaveOpen) { }
+
+		private bool isBigEndian = false;
+		public bool IsBigEndian
+		{
+			get => isBigEndian;
+			set => isBigEndian = value;
+		}
+
+		public override short ReadInt16()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadInt16BigEndian(base.ReadBytes(2));
+			else
+				return base.ReadInt16();
+		}
+
+		public override ushort ReadUInt16()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadUInt16BigEndian(base.ReadBytes(2));
+			else
+				return base.ReadUInt16();
+		}
+
+		public override int ReadInt32()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadInt32BigEndian(base.ReadBytes(4));
+			else
+				return base.ReadInt32();
+		}
+
+		public override uint ReadUInt32()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadUInt32BigEndian(base.ReadBytes(4));
+			else
+				return base.ReadUInt32();
+		}
+
+		public override long ReadInt64()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadInt64BigEndian(base.ReadBytes(8));
+			else
+				return base.ReadInt64();
+		}
+
+		public override ulong ReadUInt64()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadUInt64BigEndian(base.ReadBytes(8));
+			else
+				return base.ReadUInt64();
+		}
+
+		public override Half ReadHalf()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadHalfBigEndian(base.ReadBytes(2));
+			else
+				return base.ReadHalf();
+		}
+
+		public override float ReadSingle()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadSingleBigEndian(base.ReadBytes(4));
+			else
+				return base.ReadSingle();
+		}
+
+		public override double ReadDouble()
+		{
+			if (isBigEndian)
+				return BinaryPrimitives.ReadDoubleBigEndian(base.ReadBytes(8));
+			else
+				return base.ReadDouble();
+		}
+
+		public override decimal ReadDecimal()
+		{
+			if (isBigEndian)
+				throw new NotSupportedException();
+			else
+				return base.ReadDecimal();
+		}
+	}
+}
---
+++ MeshSharp/IO/EndianWriter.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+using System.Text;
+
+namespace MeshSharp.IO
+{
+	internal class EndianWriter : BinaryWriter
+	{
+		public EndianWriter(Stream output) : base(output) { }
+		public EndianWriter(Stream output, Encoding encoding) : base(output, encoding) { }
+		public EndianWriter(Stream output, Encoding encoding, bool leaveOpen) : base(output, encoding, leaveOpen) { }
+		protected EndianWriter() { }
+
+		private bool isBigEndian = false;
+		public bool IsBigEndian
+		{
+			get => isBigEndian;
+			set => isBigEndian = value;
+		}
+
+		public override void Write(short value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[2];
+				BinaryPrimitives.WriteInt16BigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(ushort value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[2];
+				BinaryPrimitives.WriteUInt16BigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(int value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[4];
+				BinaryPrimitives.WriteInt32BigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(uint value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[4];
+				BinaryPrimitives.WriteUInt32BigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(long value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[8];
+				BinaryPrimitives.WriteInt64BigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(ulong value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[8];
+				BinaryPrimitives.WriteUInt64BigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(Half value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[2];
+				BinaryPrimitives.WriteHalfBigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(float value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[4];
+				BinaryPrimitives.WriteSingleBigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(double value)
+		{
+			if (isBigEndian)
+			{
+				byte[] bytes = new byte[8];
+				BinaryPrimitives.WriteDoubleBigEndian(bytes, value);
+				base.Write(bytes);
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+
+		public override void Write(decimal value)
+		{
+			if (isBigEndian)
+			{
+				throw new NotSupportedException();
+			}
+			else
+			{
+				base.Write(value);
+			}
+		}
+	}
+}
