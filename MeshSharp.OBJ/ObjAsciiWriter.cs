ba62d3a1edb8853d34eff748d962d29bd238cc78
Jeremy Pritts <jrpritts7@gmail.com>
2021-11-20 17:37:00 -0500

    make obj inherit from mesh writer base

---
+++ MeshSharp.OBJ/ObjAsciiWriter.cs
@@ -0,0 +1,24 @@
+﻿using MeshSharp.Elements;
+using MeshSharp.Elements.Geometries;
+using MeshSharp.IO;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MeshSharp.OBJ
+{
+	internal class ObjAsciiWriter : MeshWriterBase
+	{
+		public ObjAsciiWriter(Stream stream) : base(stream) { }
+		public ObjAsciiWriter(string path) : base(path) { }
+
+		public void Write(Scene scene)
+		{
+			List<Mesh> meshes = MeshUtils.GetAllMeshes(scene);
+			ObjMesh processedMesh = ObjMesh.ConvertFromMeshes(meshes);
+			using (StreamWriter writer = new StreamWriter(stream))
+			{
+				writer.WriteLine(processedMesh.ToAsciiString());
+			}
+		}
+	}
+}
--- MeshSharp.OBJ/ObjWriter.cs
+++ MeshSharp.OBJ/ObjWriter.cs
@@ -1,56 +1,11 @@
 ﻿using MeshSharp.Elements;
-using MeshSharp.Elements.Geometries;
-using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace MeshSharp.OBJ
 {
-	public class ObjWriter : IDisposable
+	public static class ObjWriter
 	{
-		private bool disposedValue;
-		protected Stream stream;
-
-		public ObjWriter(Stream stream)
-		{
-			this.stream = stream;
-		}
-		public ObjWriter(string path) : this(File.Create(path)) { }
-
-		public void Write(Scene scene)
-		{
-			List<Mesh> meshes = MeshUtils.GetAllMeshes(scene);
-			ObjMesh processedMesh = ObjMesh.ConvertFromMeshes(meshes);
-			using (StreamWriter writer = new StreamWriter(stream))
-			{
-				writer.WriteLine(processedMesh.ToAsciiString());
-			}
-		}
-
-		public static void Write(string path, Scene scene) => new ObjWriter(path).Write(scene);
-		public static void Write(Stream stream, Scene scene) => new ObjWriter(stream).Write(scene);
-
-		#region Disposal
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposedValue)
-			{
-				if (disposing)
-				{
-					stream?.Dispose();
-				}
-
-				stream = null;
-				disposedValue = true;
-			}
-		}
-
-		public void Dispose()
-		{
-			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-			Dispose(disposing: true);
-			GC.SuppressFinalize(this);
-		}
-		#endregion
+		public static void Write(string path, Scene scene) => new ObjAsciiWriter(path).Write(scene);
+		public static void Write(Stream stream, Scene scene) => new ObjAsciiWriter(stream).Write(scene);
 	}
 }
